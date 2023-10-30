ff796a72eb022c53de9460e4488ed9f419c202ca
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-10-30 15:18:44 +0100

    ✨ feat(main.jsx): Add React router
    Add React router to project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from './Pages/HomePage/HomePage.jsx'
+import Login from './Pages/Login/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
